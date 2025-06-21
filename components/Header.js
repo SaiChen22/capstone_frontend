ed889e0f037dcc55dc2d1608911f031de18a7c64
Sai <saikaungsanchen@gmail.com>
2025-06-20 21:33:31 -0400

    setting up framworks

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            {/* Content will be added in later steps */}
+        </header>
+    );
+};
+
+export default Header; 
---
+++ components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <main>
+            {/* Content will be added in later steps */}
+        </main>
+    );
+};
+
+export default Main; 
