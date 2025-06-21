b5c8fe08cd3cf1b2a1a2bea204ee64086a208d0c
Sai <saikaungsanchen@gmail.com>
2025-06-20 21:43:45 -0400

    setting up wireframes

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,16 +2,8 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <header className="hero-section">
-            <div className="hero-text">
-                <h1>Little Lemon</h1>
-                <h2>Chicago</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                <button>Reserve a Table</button>
-            </div>
-            <div className="hero-image">
-                {/* Image will be added here */}
-            </div>
+        <header>
+            {/* Content will be added in later steps */}
         </header>
     );
 };
